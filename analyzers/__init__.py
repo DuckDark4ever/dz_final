d0041e9db307ce77dd63ea7abb0e62d40ac3d3b2
DuckDark4ever <lukyanovmv21@oiate.ru>
2026-03-11 19:12:38 +0300

    Create __init__.py

---
+++ analyzers/__init__.py
@@ -0,0 +1,12 @@
+"""
+Пакет для анализа данных и выявления угроз.
+"""
+from analyzers.base import BaseAnalyzer
+from analyzers.cvss_analyzer import CVSSAnalyzer
+from analyzers.traffic_analyzer import TrafficAnalyzer
+
+__all__ = [
+    'BaseAnalyzer',
+    'CVSSAnalyzer',
+    'TrafficAnalyzer'
+]
